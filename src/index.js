6ffee6e377d1fd4c60b867fa34b83d787e7fb109
Sofia Velasquez <s.velasquezm2@uniandes.edu.co>
2023-09-26 09:20:49 -0500

    90% done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {IntlProvider} from 'react-intl';
+import localeEsMessages from "./locales/es";
+import localeEnMessages from "./locales/en";
 
-import JobsList from "./components/jobslist";
 
-ReactDOM.render(<JobsList />, document.getElementById("root"));
+import JobsList from "./components/jobsList";
+
+const navegatorLang = navigator.language;
+const messages = navegatorLang.includes("es") ? localeEsMessages : localeEnMessages;
+
+ReactDOM.render(
+    <IntlProvider locale={navegatorLang} messages= {messages}>
+            <JobsList/>
+    </IntlProvider>, document.getElementById("root")
+);
